65bb2af60444448c203dc8c7b9f0098425d36bad
Rob Allen <tellutwurp@gmail.com>
2015-03-27 13:45:20 -0600

    removing older files

--- Assets/Scripts/Enemy/EnemyFighterAIBehavior.cs
+++ Assets/Scripts/Enemy/EnemyFighterAIBehavior.cs
@@ -13,51 +13,51 @@ namespace Assets.Scripts.Emeny
 		public float FireRate;
 		public Rigidbody Weapon;
 
-		private float nextFire = 3.0f;
+		private float nextFire = 2.0f;
 
 		public EnemyFighterAIBehavior ()
 		{
+
 		}
 
-		public void AcquireTarget() {
+		public void AcquireTarget ()
+		{
 			var target = GameObject.FindGameObjectsWithTag ("Player").
 				OrderBy (go => Vector3.Distance (go.transform.position, transform.position)).FirstOrDefault ();
 			 
-	         float dist = Vector3.Distance(target.transform.position,transform.position);
+			float dist = Vector3.Distance (target.transform.position, transform.position);
 	         
+			if (dist <= Range) {
+				var direction = (transform.position - target.transform.position).normalized;
+				var rotation = Quaternion.LookRotation (direction);
 
-			if(dist <= Range){
-				var direction = (transform.position - target.transform.position ).normalized;
-				var rotation = Quaternion.LookRotation(direction);
-
-				if ( Time.time > nextFire )
-				{
-
-					Rigidbody bullet = (Rigidbody) Instantiate(Weapon, transform.position, Quaternion.identity);
+				if (Time.time > nextFire) {
+					Rigidbody bullet = (Rigidbody)Instantiate (Weapon, transform.position, Quaternion.identity);
 					
-					bullet.GetComponent<DestroyByContactBehavior>().Owner = gameObject.GetInstanceID();// ProjectileOwner.Self;
-					bullet.transform.Rotate(rotation.eulerAngles + new Vector3(0,90,0));
-					bullet.GetComponent<Rigidbody>().AddForce(1000f * (target.transform.position -transform.position  ).normalized);
-					nextFire = Time.time + UnityEngine.Random.Range(0.5f,1.0f);
-
+					bullet.GetComponent<DestroyByContactBehavior> ().Owner = gameObject.GetInstanceID ();// ProjectileOwner.Self;
+					bullet.transform.Rotate (rotation.eulerAngles + new Vector3 (0, 90, 0));
+					bullet.GetComponent<Rigidbody> ().AddForce (1000f * (target.transform.position - transform.position).normalized);
+					nextFire = Time.time + UnityEngine.Random.Range (1.0f, 2.0f);
 				}
 
-				if ( dist > 10 )
-				{
-					transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 30);
-					transform.position = Vector3.MoveTowards(transform.position, target.transform.position, 8 * Time.deltaTime);
+				if (dist > 10) {
+					transform.rotation = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * 30);
+					transform.position = Vector3.MoveTowards (transform.position, target.transform.position, 8 * Time.deltaTime);
 				}
 			}
-	         // Do Something
 		}
 
-		void Update() {
+		void Update ()
+		{
 			AcquireTarget ();
 		}
 
-		public void DamageTarget() {}
+		public void DamageTarget ()
+		{
+		}
 
-		public void ChaseTarget() {
+		public void ChaseTarget ()
+		{
 
 		}
 
